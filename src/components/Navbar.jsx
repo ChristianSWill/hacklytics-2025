d39e84730f108da249b6bbdd75f55751b4bb1ea9
ChristianSWill <Christian.SWillingham@gmail.com>
2025-02-23 03:57:12 -0500

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+import "../App.css";
+import { NavLink } from "react-router";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar">
+      <h1 className="nav-header">Healthy Hackers</h1>
+      <ul className="navbar-list">
+        <li>
+          <NavLink to="/">Diabetes</NavLink>
+        </li>
+        <li>
+          <NavLink to="/stroke">Stroke</NavLink>
+        </li>
+        <li>
+          <NavLink to="/cardiovascular">Heart Disease</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
